a9f3b0bf5f78d64a4aa63d0fc806ff97194e1116
Kim Do Hyeong <kdh@m4nd4r1n.me>
2024-03-08 23:48:19 +0900

    chore: change blog config for demo

--- blog.config.ts
+++ blog.config.ts
@@ -1,12 +1,13 @@
 import { blogConfig } from './src/libs/blog-config';
 
 export default blogConfig({
-  title: 'Devlog.',
+  title: 'Next.js notion blog',
   author: 'm4nd4r1n',
   email: 'kdh@m4nd4r1n.me',
-  siteUrl: 'https://blog.m4nd4r1n.me',
-  description: "m4nd4r1n's Devlog.",
-  locale: 'ko-KR',
+  siteUrl: 'https://blog-demo.m4nd4r1n.me',
+  description:
+    'A Next.js, Tailwind CSS blog template, based on Next.js App Router and using react-notion-x to render notion posts.',
+  locale: 'en-US',
   timezone: 'Asia/Seoul',
   theme: 'system',
   postsPerPage: 4,
@@ -16,21 +17,21 @@ export default blogConfig({
   socialLink: 'https://github.com/m4nd4r1n',
   lightBg: '#ffffff',
   darkBg: '#18181b',
-  comments: {
-    provider: 'giscus',
-    giscusConfig: {
-      repo: 'm4nd4r1n/blog-comment',
-      repoId: 'R_kgDOKHzBrw',
-      category: 'Announcements',
-      categoryId: 'DIC_kwDOKHzBr84CYpHD',
-      mapping: 'pathname',
-      strict: '0',
-      reactionsEnabled: '1',
-      emitMetadata: '0',
-      inputPosition: 'top',
-      lang: 'ko',
-    },
-  },
+  // comments: {
+  //   provider: 'giscus',
+  //   giscusConfig: {
+  //     repo: '',
+  //     repoId: '',
+  //     category: 'Announcements',
+  //     categoryId: '',
+  //     mapping: 'pathname',
+  //     strict: '0',
+  //     reactionsEnabled: '1',
+  //     emitMetadata: '0',
+  //     inputPosition: 'top',
+  //     lang: 'ko',
+  //   },
+  // },
   analytics: {
     googleAnalytics: {
       googleAnalyticsId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? '',
